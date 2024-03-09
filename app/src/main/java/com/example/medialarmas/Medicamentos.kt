56f44851981f36dfbc427b536bc952fabcca5e6f
Hernan Alvarez Caballero <hd.alvarez@uniandes.edu.co>
2024-03-09 12:09:39 -0500

    Add new activity and the change view

--- app/src/main/java/com/example/medialarmas/Medicamentos.kt
+++ app/src/main/java/com/example/medialarmas/Medicamentos.kt
@@ -1,27 +1,20 @@
 package com.example.medialarmas
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class Medicamentos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_medicamentos)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val btnMedicamentos = findViewById<Button>(R.id.button2)
-        btnMedicamentos.setOnClickListener {
-            val intent = Intent(this@MainActivity, Medicamentos::class.java)
-            startActivity(intent)
         }
     }
 }
